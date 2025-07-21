f9886e209cdfb9be7d2897ccb61cad47db3e8be3
stevegnr <steve.garneri@hotmail.fr>
2025-07-21 14:48:28 +0200

    ajout style si cellule fermée

--- apps/web/src/components/Cell.tsx
+++ apps/web/src/components/Cell.tsx
@@ -10,11 +10,18 @@ export type CellType = {
 };
 
 const Cell = ({ cell }: Props) => {
+  const { isOpen, hasBomb, bombsAround } = cell;
+
   return (
-    <div className="border-2 w-10 h-10 rounded-md text-center text-2xl">
-      {cell.hasBomb ? "💣" : cell.bombsAround}
+    <div
+      className={`
+        border-2 w-10 h-10 rounded-md text-center text-2xl
+        ${isOpen ? "bg-white" : "bg-gray-400 cursor-pointer"}
+      `}>
+      {isOpen ? (hasBomb ? "💣" : bombsAround) : ""}
     </div>
   );
 };
+
 
 export default Cell;
