335fb3a31144a273b7fc1364faff51e20196a26e
stevegnr <steve.garneri@hotmail.fr>
2025-07-22 11:13:06 +0200

    style

--- apps/web/src/components/Cell.tsx
+++ apps/web/src/components/Cell.tsx
@@ -16,14 +16,49 @@ const Cell = ({ cell, onPlayMove }: Props) => {
     onPlayMove(cell);
   };
 
+  let textColor: string;
+
+  switch (bombsAround) {
+    case 1:
+      textColor = "blue";
+      break;
+    case 2:
+      textColor = "green";
+      break;
+    case 3:
+      textColor = "red";
+      break;
+    case 4:
+      textColor = "orange";
+      break;
+    case 5:
+      textColor = "yellow";
+      break;
+    case 6:
+      textColor = "violet";
+      break;
+    case 7:
+      textColor = "brown";
+      break;
+    case 8:
+      textColor = "black";
+      break;
+
+    default:
+      textColor = "white";
+      break;
+  }
+
   return (
     <div
       onClick={handleClick}
       className={`
-        border-2 w-10 h-10 rounded-md text-center text-2xl
+        border-2 border-gray-600 w-10 h-10 rounded-md text-center text-2xl font-bold
         ${isOpen ? "bg-white" : "bg-gray-400 cursor-pointer"}
-      `}>
-      {isOpen ? (hasBomb ? "💣" : bombsAround) : ""}
+        
+      `}
+      style={{ color: isOpen ? textColor : undefined }}>
+      {isOpen ? (hasBomb ? "💥" : bombsAround) : ""}
     </div>
   );
 };
